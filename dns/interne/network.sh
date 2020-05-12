25f507102358878da1a374a483b854a0e1940350
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2020-05-12 18:30:43 +0200

    Update network.sh

--- dns/interne/network.sh
+++ dns/interne/network.sh
@@ -1,2 +1,7 @@
 #! /bin/sh
-sudo docker network create -d bridge intranet
+docker network create \
+--driver=bridge \
+--subnet=172.16.0.0/24 \
+--ip-range=172.16.0.0/24 \
+--gateway=172.16.0.254/24 \
+intranet
