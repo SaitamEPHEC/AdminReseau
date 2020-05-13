1385b8ef04a820f616e56251c71b7b1d0d11f2b5
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2020-05-13 03:27:58 +0200

    Update network.sh

--- users/network.sh
+++ users/network.sh
@@ -1,17 +1,22 @@
 #! /bin/sh
-# --driver=bridge \
+
 sudo docker network create \
+--driver=bridge \
 --subnet=172.16.2.0/24 \
 --ip-range=172.16.2.0/24 \
 --gateway=172.16.2.254 \
 administration
 sudo docker network create \
+--driver=bridge \
 --subnet=172.16.3.0/24 \
 --ip-range=172.16.3.0/24 \
 --gateway=172.16.3.254 \
 direction
 sudo docker network create \
+--driver=bridge \
 --subnet=172.16.4.0/24 \
 --ip-range=172.16.4.0/24 \
 --gateway=172.16.4.254 \
 atelier
+docker network connect administration userTest
+docker network connect intranet userTest
