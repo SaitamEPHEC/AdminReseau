b5a703c2e8d055916a076cae42cf5056fd351d29
Gassmann Mathias <43479118+SaitamEPHEC@users.noreply.github.com>
2020-05-13 03:39:41 +0200

    Update network.sh

--- dns/interne/network.sh
+++ dns/interne/network.sh
@@ -5,4 +5,4 @@ sudo docker network create \
 --ip-range=172.16.0.0/24 \
 --gateway=172.16.0.254 \
 intranet
-docker network connect intranet resolveur
+docker network connect intranet resolveur --ip 172.16.2.1
